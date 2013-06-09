46df12f2c272b384ea42590d9bdfaea5008e63f0
David Worley <dave@instructables.com>
2013-06-09 12:10:15 -0700

    Wiring up the session

--- class_sessions/views.py
+++ class_sessions/views.py
@@ -1,4 +1,6 @@
+import OpenTokSDK
 from django.views.generic import TemplateView
+from django.conf import settings
 
 class ClassProfileView(TemplateView):
     template_name = 'classes/class_profile.html'
@@ -31,10 +33,25 @@ class ClassSessionView(TemplateView):
     template_name = 'classes/class_session.html'
 
     def get(self, request, *args, **kwargs):
+        self.opentok_sdk = OpenTokSDK.OpenTokSDK(settings.OPENTOK_API_KEY, settings.OPENTOK_PRIVATE_KEY)
         kwargs['logged_in'] = True
         kwargs['counter'] = range(0,4)
+        kwargs['opentok_session'] = self.make_opentok_session()
         response = super(ClassSessionView, self).get(request, *args, **kwargs)
         return response
+
+    def make_opentok_session(self):
+        session_properties = {
+            OpenTokSDK.SessionProperties.p2p_preference: "enabled"
+        }
+        return self.opentok_sdk.create_session(None, session_properties)
+
+    def get_token_for_session(self, session, request):
+        role_constants = OpenTokSDK.RoleConstants
+        connectionMetadata = "username=%s" % (request.user.username)
+        token = self.opentok_sdk.generate_token(session.session_id, role_constants.PUBLISHER, None, connectionMetadata)
+        return token
+
 
 class CreateClassView(TemplateView):
     template_name = 'classes/create_class.html'
